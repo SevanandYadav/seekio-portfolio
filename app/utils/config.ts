a53cf28e9bf6e82bd529d4b477920294ff41489f
SevanandYadav <sevanandyadav@gmail.com>
2025-12-07 12:26:45 +0530

    cong

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,9 +1,7 @@
 // Content source configuration
 const GITHUB_RAW_BASE = "https://raw.githubusercontent.com/SevanandYadav/seekio-portfolio/data";
 
-export const CONTENT_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? GITHUB_RAW_BASE 
-  : '';
+export const CONTENT_BASE_URL = GITHUB_RAW_BASE;
 
 export const getContentUrl = (path: string) => {
   return `${CONTENT_BASE_URL}/content${path}`;
