2090171a4447beba0de81232eae12e717719ebb6
SevanandYadav <sevanandyadav@gmail.com>
2025-12-07 09:18:04 +0530

    netlify router

--- netlify/functions/server.mjs
+++ netlify/functions/server.mjs
@@ -1,7 +1,2 @@
-import { createRequestHandler } from "@react-router/node";
-import * as build from "../../build/server/index.js";
-
-export const handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
+import handler from "../../build/server/index.js";
+export { handler };
